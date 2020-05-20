a38a5c36c3fb1a3035971a69c4003e181ad93e66
weiyupeng <weiyupengself@163.com>
2020-05-20 15:00:30 +0800

    [20200520]

--- src/components/Content.vue
+++ src/components/Content.vue
@@ -60,6 +60,229 @@
         </li>
       </ul>
     </div>
+    <div class="main_loop">
+      <ul class="main_loop_ul">
+        <li class="main_li">
+          <div class="ajax_content">
+            <a href="javascript:;" class="main_title" target="_blank">去过济州岛，才明白什么叫“天然鲜”</a>
+            <div class="main_others">
+              <a class="main_cat" href="javascript:;">旅途见闻 /  food</a>
+              <a class="main_actor" target="_blank" href="javascript:;">MR.Jack</a>
+              写在2015年03月30
+            </div>
+            <div class="main_fenge"></div>
+            <div class="main_excerp">
+              <p class="index_p_d">听到我要去济州岛，对方的反应大多是“济州岛有什么好玩的。”虽说不看韩剧也不哈韩流，更不买韩妆，可谁让人家好吃关键还免签证，齐备了一场说吃就吃的旅行两大关键元素。然而这么胸有成竹的样子都是装出来的。两年前去首尔的时候觉得韩国吃食简直乏善可陈，满大街捧着咖啡杯艳丽冻人的美少女，然而咖啡和甜品都不够美味，被日本甩出不知道多少条街。传统韩国菜又太不适合一个人吃，曾在首尔明洞要了一份参鸡汤，上来整只乌骨...</p>
+            </div>
+            <a class="readmore" rel=""><span>展开全部</span>read more</a>
+            <div class="readloading"></div>
+            <div class="main_photos">
+              <img src="../assets/img/main1.jpg">
+            </div>
+            <div class="moretag">
+              <span class="main_tag"></span>
+            </div>
+          </div>
+        </li>
+        <li class="main_li">
+          <div class="ajax_content">
+            <a href="javascript:;" class="main_title" target="_blank">去过济州岛，才明白什么叫“天然鲜”</a>
+            <div class="main_others">
+              <a class="main_cat" href="javascript:;">旅途见闻 /  food</a>
+              <a class="main_actor" target="_blank" href="javascript:;">MR.Jack</a>
+              写在2015年03月30
+            </div>
+            <div class="main_fenge"></div>
+            <div class="main_excerp">
+              <p class="index_p_d">听到我要去济州岛，对方的反应大多是“济州岛有什么好玩的。”虽说不看韩剧也不哈韩流，更不买韩妆，可谁让人家好吃关键还免签证，齐备了一场说吃就吃的旅行两大关键元素。然而这么胸有成竹的样子都是装出来的。两年前去首尔的时候觉得韩国吃食简直乏善可陈，满大街捧着咖啡杯艳丽冻人的美少女，然而咖啡和甜品都不够美味，被日本甩出不知道多少条街。传统韩国菜又太不适合一个人吃，曾在首尔明洞要了一份参鸡汤，上来整只乌骨...</p>
+            </div>
+            <a class="readmore" rel=""><span>展开全部</span>read more</a>
+            <div class="readloading"></div>
+            <div class="main_photos">
+              <div id="gallerys" data-pswp-uid="3">
+                <a title="1111111111" data-author="by.MR.Jack" data-size="832x554" data-med-size="832x554" data-med="" href="">              
+                  <img width="150" height="150" alt="1111111111" class="attachment-gallery_lightbox size-gallery_lightbox" src="../assets/img/k11.jpg">
+                  <figure>大峡谷美景如画，山峰清凉，空气清新</figure>
+                </a>
+                <a title="1111111111" data-author="by.MR.Jack" data-size="832x554" data-med-size="832x554" data-med="" href="">
+                  <img width="150" height="150" alt="1111111111" class="attachment-gallery_lightbox size-gallery_lightbox" src="../assets/img/k11.jpg">
+                  <figure>大峡谷美景如画，山峰清凉，空气清新</figure>
+                </a>
+                <a title="1111111111" data-author="by.MR.Jack" data-size="480x602" data-med-size="480x602" data-med="" href="">
+                  <img width="150" height="150" alt="1111111111" class="attachment-gallery_lightbox size-gallery_lightbox" src="../assets/img/k11.jpg">
+                  <figure>大峡谷美景如画，山峰清凉，空气清新</figure>
+                </a>
+                <a title="1111111111" data-author="by.MR.Jack" data-size="903x582" data-med-size="903x582" data-med="" href="">
+                  <img width="150" height="150" alt="1111111111" class="attachment-gallery_lightbox size-gallery_lightbox" src="../assets/img/k11.jpg">
+                  <figure>大峡谷美景如画，山峰清凉，空气清新</figure>
+                </a>
+                <a title="1111111111" data-author="by.MR.Jack" data-size="903x582" data-med-size="903x582" data-med="" href="">
+                  <img width="150" height="150" alt="1111111111" class="attachment-gallery_lightbox size-gallery_lightbox" src="../assets/img/k11.jpg">
+                  <figure>大峡谷美景如画，山峰清凉，空气清新</figure>
+                </a>
+                <a title="1111111111" data-author="by.MR.Jack" data-size="903x582" data-med-size="903x582" data-med="" href="">  
+                  <img width="150" height="150" alt="1111111111" class="attachment-gallery_lightbox size-gallery_lightbox" src="../assets/img/k11.jpg">
+                  <figure>大峡谷美景如画，山峰清凉，空气清新</figure>
+                </a>
+                <a title="1111111111" data-author="by.MR.Jack" data-size="903x582" data-med-size="903x582" data-med="" href="">
+                  <img width="150" height="150" alt="1111111111" class="attachment-gallery_lightbox size-gallery_lightbox" src="../assets/img/k11.jpg">
+                  <figure>大峡谷美景如画，山峰清凉，空气清新</figure>
+                </a>
+                <a title="1111111111" data-author="by.MR.Jack" data-size="903x582" data-med-size="903x582" data-med="" href="">
+                  <img width="150" height="150" alt="1111111111" class="attachment-gallery_lightbox size-gallery_lightbox" src="../assets/img/k11.jpg">
+                  <figure>大峡谷美景如画，山峰清凉，空气清新</figure>
+                </a>
+                
+              </div>
+            </div>
+            <div class="moretag">
+              <span class="main_tag"></span>
+            </div>
+          </div>
+        </li>
+      </ul>
+    </div>
+    <div class="sidbar">
+        <div class="widget widget_calendar" id="calendar-2">
+            <h2>日历</h2>
+            <div class="calendar_wrap" id="calendar_wrap">
+                <table id="wp-calendar">
+                    <caption>{{date_year}}年{{date_month}}月</caption>
+                    <thead>
+                        <tr>
+                            <th title="星期一" scope="col">一</th>
+                            <th title="星期二" scope="col">二</th>
+                            <th title="星期三" scope="col">三</th>
+                            <th title="星期四" scope="col">四</th>
+                            <th title="星期五" scope="col">五</th>
+                            <th title="星期六" scope="col">六</th>
+                            <th title="星期日" scope="col">日</th>
+                        </tr>
+                    </thead>
+                    <tfoot>
+                        <tr>
+                            <td id="prev" colspan="3"><a href="javascript:;">« 4月</a></td>
+                            <td class="pad">&nbsp;</td>
+                            <td class="pad" id="next" colspan="3">&nbsp;</td>
+                        </tr>
+                    </tfoot>
+                    <tbody>
+                        <tr>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                        </tr>
+                        <tr>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                        </tr>
+                        <tr>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                        </tr>
+                        <tr>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                        </tr>
+                        <tr>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                        </tr>
+                        <tr>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+        <div class="widget">
+            <h2><a href=""> 关于我</a></h2>  
+            <a target="_blank" href="javascript:;" class="about_pic"><img alt="关于我" src="../assets/img/me.jpg"></a>     
+            <div class="code_s about_p">
+                <strong>MR.Jack</strong><br>
+                18世纪在世界史上是个特别迷人的世纪，它将启蒙运动紧紧拥在怀中。/<br>
+                这是最好的世界 /<br>
+                也是最坏的世界 /<br>
+                旅途路上永远会遇到有趣的事情<br>
+            </div>
+        </div>
+        <div class="fixed_mok">
+            <div id="two_show" class="news_tuoch widget "> 
+                <h2><a href="javascript:;"> post</a></h2> 
+                <ul>
+                    <li>
+                        <a target="_blank" href="javascript:;" class="news_pics">
+                            <img width="307" height="204" title="阳光僧林" alt="阳光森林" class="attachment-case size-case wp-post-image" src="../assets/img/cpss1.jpg">
+                        </a>
+                        <div class="news_textss">
+                            <a title="" target="_blank" href="javascript:;">夏日午后的突发事件</a>
+                            <em>2016/01/28</em>
+                            <p>像所有周日那样，苏先生在家陪着我，或者说，我陪着他。这件事情有些说不...</p>
+                        </div>
+                    </li>     
+                    <li>
+                        <a target="_blank" href="javascript:;" class="news_pics"><img width="307" height="204" title="阳光僧林" alt="阳光森林" class="attachment-case size-case wp-post-image" src="../assets/img/cpss1.jpg"></a>
+                        <div class="news_textss">
+                            <a title="" target="_blank" href="javascript:;">夏日午后的突发事件</a>
+                            <em>2016/01/28</em>
+                            <p>像所有周日那样，苏先生在家陪着我，或者说，我陪着他。这件事情有些说不...</p>
+                        </div>
+                    </li>     
+                    <li>
+                        <a target="_blank" href="javascript:;" class="news_pics"><img width="307" height="204" title="阳光僧林" alt="阳光森林" class="attachment-case size-case wp-post-image" src="../assets/img/cpss1.jpg"></a>
+                        <div class="news_textss">
+                            <a title="" target="_blank" href="javascript:;">夏日午后的突发事件</a>
+                            <em>2016/01/28</em>
+                            <p>像所有周日那样，苏先生在家陪着我，或者说，我陪着他。这件事情有些说不...</p>
+                        </div>
+                    </li>    
+                    <li>
+                        <a target="_blank" href="javascript:;" class="news_pics"><img width="307" height="204" title="阳光僧林" alt="阳光森林" class="attachment-case size-case wp-post-image" src="../assets/img/cpss1.jpg"></a>
+                        <div class="news_textss">
+                            <a title="" target="_blank" href="javascript:;">夏日午后的突发事件</a>
+                            <em>2016/01/28</em>
+                            <p>像所有周日那样，苏先生在家陪着我，或者说，我陪着他。这件事情有些说不...</p>
+                        </div>
+                    </li>           
+                </ul> 
+            </div>
+            <div class="widget">
+                <div class="code_s">
+                    <a href="javascript:;" target="_blank"><img alt="关于我" src="../assets/img/ad.jpg"></a>
+                </div>
+            </div> 
+        </div>
+    </div>
   </div>
 </template>
 
@@ -71,7 +294,11 @@ export default {
     },
     data(){
         return {
-            
+            date_year:0,
+            date_month:12,
+            today:0,
+            showthirty:true,
+            showthirtyone:false
         }
     },
     name: 'Content',
@@ -85,6 +312,32 @@ export default {
          
           $(this).children('.recommend_list').css('opacity','0');
         })
+      },
+      date(){
+        this.date_year = new Date().getFullYear();
+        /* this.date_month = new Date().getMonth()+1; */
+        this.showthirty = this.date_month == 2 ? false : true;
+        this.showthirtyone = this.date_month == 2 || this.date_month == 4 || this.date_month == 6 ||this.date_month == 6 ||this.date_month == 11 ? false :true ;
+        switch(this.date_month)
+          {
+              case 2:
+                  this.showthirtyone = false;
+                  break;
+              case 4:
+                  this.showthirtyone = false;
+                  break;
+              case 6:
+                  this.showthirtyone = false;
+                  break;
+              case 9:
+                 this.showthirtyone = false;
+                  break;
+              case 11:
+                  this.showthirtyone = false;
+                  break;
+              default:
+                  this.showthirtyone = true;
+          }
       }
     },
     created() {
@@ -92,17 +345,18 @@ export default {
     },
     mounted() {
       this.mouseover();
+      this.date();
     },
 }
 </script>
 
 <style scoped>
-  .content{width: 11.2rem;margin: 30px auto 0;}
+  .content{width: 98%;max-width: 1180px;margin: 30px auto 0;}
   .recommend{width: 100%;background: #fff;box-shadow: 0 0 3px #999999;height: auto;overflow: hidden;}
   .recommend ul li{float: left; margin: 1%; display: block; text-align: center;}
   .recommend_img img, .recommend_img, .recommend_img .btn,.recommend_list{transition: all .5s;-moz-transition: all 0.5s;-webkit-transition: all 0.5s;-o-transition: all 0.5s;}
   .recommend_list{opacity:0;width: 80%;height: 100%;padding: 0 10%;background: rgba(0,0,0,.3);position: absolute;top: 0;left: 0;font-size: 12px;color: #fff;text-align: left;z-index: 2;}
-  .recommend_list p:first-letter{float: left;font-size:30px; font-weight:bold;padding-right:5px;}
+  .recommend_list p::first-letter,.index_p_d::first-letter{float: left;font-size:30px; font-weight:bold;padding-right:5px;}
   .recommend_list .btn:hover{ color:#ff7200;border:solid 1px #ff7200; background:rgba(0,0,0,.3);}
   .recommend ul li .recommend_img:hover img{-webkit-filter: blur(5px);-moz-filter: blur(5px); -o-filter: blur(5px);  -ms-filter: blur(5px); filter: blur(5px);}
   @media screen and (max-width: 2000px) and (min-width: 1024px){
@@ -111,9 +365,9 @@ export default {
     }
   }
   @media screen and (max-width: 1023px) and (min-width: 100px){
-    .recommend ul li {
-      width: 245px;
-    }
+    .recommend ul li {width: 245px;}
+    .sidbar{ display:none;}
+	  .main_loop{ width:100%;}
   }
   .recommend_img{font-size: 0;overflow: hidden; display: block; position: relative;}
   .recommend_img img{position: relative;z-index: 1;}
@@ -124,4 +378,53 @@ export default {
   .recommend ul li .cat {font-size: 12px; color: #0066a6; transition: all 0.8s;  -moz-transition: all 0.8s;-webkit-transition: all 0.8s;-o-transition: all 0.8s;}
   .recommend ul li .actor, .recommend ul li .actor a {font-size: 12px;color: #666;}
   .recommend ul li .recommend_title a {font-size: 16px;color: #666;height: 24px;overflow: hidden;margin-top: 5px;}
+
+  /* main_loop */
+  .main_loop {float: left;width: 65%;height: auto;margin-top: 20px;}
+  .main_loop ul .main_li, .main_loop ul .mains_li {width: 99%;padding: 15px 0.5%;height: auto;background: #fff;box-shadow: 0 0 3px #999;overflow: hidden;float: left;margin-bottom: 20px;}
+  .main_title {display: block;width: 95%;height: auto;font-size: 18px;text-align: center;padding: 5px 2.5% 15px 2.5%;color: #333;font-weight: bold;}
+  .main_others {width: 95%;height: auto;padding: 0 2.5% 20px 2.5%;text-align: center;font-size: 12px;color: #8e8e8e;}
+  .main_others a {display: inline-block;font-size: 12px;background-image: url(../assets/img/icon.png);background-repeat: no-repeat;}
+  .main_fenge {width: 100%;height: 24px;background: no-repeat left url(../assets/img/fenge.png);}
+  .main_excerp {width: 95%;height: auto;padding: 0 2.5% 0 2.5%;text-align: left;display: block;font-size: 12px;margin: 20px 0;color: #666;}
+  .readmore {width: 100%;height: auto;text-align: center;display: block;float: left;color: #ffba00;font-size: 12px;padding: 5px 0;line-height: 32px;margin-bottom: 10px;background: #F6F6F6;cursor: pointer;}
+  .readloading {width: 100%;height: 32px;background: center no-repeat url(../assets/img/preloader.gif);float: left;margin-bottom: 10px;display: none;}
+  .main_photos, #gallerys {width: 95%;height: auto;padding: 0 2.5% 0 2.5%;text-align: center;display: block;overflow: hidden;}
+  #gallerys a {float: left;display: block;width: 24%;padding: 0.5%;}
+  #gallerys figure {display: none;}
+  .moretag {width: 95%;height: auto;padding: 0 2.5% 0 2.5%;}
+  .main_others .main_cat {color: #0066a6;background-position: 0 3px;padding: 0 0 0 15px;margin-right: 25px;}
+  .main_others .main_actor {color: #8e8e8e;background-position: 0 -19px;padding: 0 0 0 15px;}
+  .readmore span {color: #666;margin-right: 5px;}
+  .main_photos img {display: inline-block;max-width: 100%;height: auto;}
+  .main_tag {float: right;display: block;font-size: 12px;color: #999;padding-top: 37px;}
+
+
+  /* sidbar */
+  .sidbar{ float:right; width:32%; height:auto; margin-top:20px;}
+  .widget{ width:100%;  background:url(""); border:solid 1px #eaeaea; margin-bottom:20px;}
+  .widget h2,.relevat_title{ display:block;width:95%; padding:5px 2.5%; margin-bottom:10px; border-bottom:solid 1px #eaeaea; font-size:12px; color:#3a3a3a; font-weight:normal;}
+  .widget ul,.code_s,#relevat{ display:block;width:95%; padding:5px 2.5%; overflow:hidden;}
+  #relevat{ background:url(""); border:solid 1px #eaeaea; margin-bottom:20px;}
+  .widget ul li{ width:100%; float:left; display:block; margin-bottom:10px;}
+  .widget ul li a{ padding-left:18px; color:#666; background:0 -59px no-repeat url("../assets/img/icon.png");margin-left:10px}
+  .widget ul li:hover a{ color:#0066a6; margin-left:20px;}
+  #wp-calendar{ width:100%; padding:5px 0;}
+  #wp-calendar tr,#wp-calendar td,#wp-calendar caption{ padding:5px 0; text-align:center; color:#666;}
+  #wp-calendar td#today{ font-weight:bold; color:#F60;}
+  #two_show ul li .news_pics, #relevat li .news_pics {width: 30%;float: left;margin: 0 !important;font-size: 0;}
+  .news_tuoch ul li{ display:block; float:left; width:100%;} 
+  .news_tuoch ul li a{ padding:0; display: inline-block; background:none; width:100%; text-align:center; margin:0;}
+  .news_textss{ width:100%; height:auto; text-align:center;}
+  .news_tuoch ul li .news_textss a{ display:block; width:100%; text-align:center; font-size:14px; color:#333;}
+  #relevat li .news_textss a{display:block; width:100%; font-size:14px; color:#333;}
+  .news_tuoch ul li .news_textss em{ font-size:12px; color:#CCC; display:block; width:100%;}
+  #two_show ul li  .news_pics,#relevat li .news_pics{ width:30%; float:left; margin:0 !important; font-size:0;}
+  #two_show ul li .news_textss,#relevat li .news_textss{ width:65%; float:left; margin-left:5%; }
+  #two_show ul li .news_textss a,#relevat li .news_textss a,#two_show  ul li .news_textss em,#relevat li .news_textss em{ text-align:left; margin:0 !important;}
+  #two_show ul li .news_textss p,#relevat li .news_textss p{ font-size:12px; color:#999; text-align:left;}
+  .fix_now{ position:fixed;}
+  .about_pic{ display:block; width:100%; text-align:center; padding:10px 0; font-size:0; overflow:hidden;}
+  .about_p{ width:95%; padding:0 2.5% 20px 2.5%;; display:block; font-size:12px; color:#666; }
+  .wp-post-image{width: 100%;height: auto;}
 </style>
